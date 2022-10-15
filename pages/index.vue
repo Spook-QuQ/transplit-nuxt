4e74a666ae69da8650343528bd399cb92a926734
spook-QuQ <59325698+spook-QuQ@users.noreply.github.com>
2022-10-15 13:50:02 +0900

    mini fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,30 @@
 <template lang="pug">
-  v-card.py-16
-    v-card-title.justify-center.text--secondary Transplit
-    v-divider
-    v-card-text
-      v-row
-        //- v-col.d-flex.justify-center(cols="12")
-          //- video#preview(
-          //-   autoplay
-          //-   loop
-          //-   muted
-          //- )
-          //-   source(
-          //-     src="/misc/transplit-preview.mp4"
-          //-   )
-        v-col(cols="12").d-flex.justify-center
-          div
-            h3.my-4 単語も全て翻訳する「Transplit」
-            p Transplitは文章全体の翻訳だけでなく、入力された文章を解析し単語の翻訳も行います。
-            p 単語の詳細を調べやすくするため、すぐにWeblioとjishoが開けるリンクも用意しました。
-    v-divider
-    v-card-actions.justify-center.pt-8
-      v-btn.px-5(
-        style="text-transform: none"
-        to="/translate"
-        color="primary"
-      ) Use Transplit
+v-card.py-16
+  v-card-title.justify-center.text--secondary Transplit
+  v-divider
+  v-card-text
+    v-row
+      //- v-col.d-flex.justify-center(cols="12")
+        //- video#preview(
+        //-   autoplay
+        //-   loop
+        //-   muted
+        //- )
+        //-   source(
+        //-     src="/misc/transplit-preview.mp4"
+        //-   )
+      v-col(cols="12").d-flex.justify-center
+        div
+          h3.my-4 単語も全て翻訳する「Transplit」
+          p Transplitは文章全体の翻訳だけでなく、入力された文章を解析し単語の翻訳も行います。
+          p 単語の詳細を調べやすくするため、すぐにWeblioとjishoが開けるリンクも用意しました。
+  v-divider
+  v-card-actions.justify-center.pt-8
+    v-btn.px-5(
+      style="text-transform: none"
+      to="/translate"
+      color="primary"
+    ) Use Transplit
 </template>
 
 <script>
--- pages/info.vue
+++ pages/info.vue
@@ -1,19 +1,11 @@
 <template lang="pug">
-  v-card
-    v-card-title.d-flex.justify-center.py-12 Transplit
-    v-divider
-    //- v-subheader.justify-center Made by
-    v-card-subtitle.d-flex.justify-center.pb-2 Made by
-    v-card-text.d-flex.justify-center.pt-0.pb-7
-      v-btn(
-        link
-        href="https://twitter.com/S_p_o_o_k"
-        style="text-transform: none"
-        color="primary"
-        target="_blank"
-        rel="noopener noreferrer"
-        small
-      ) @S_p_o_o_k
+v-card
+  v-card-title.d-flex.justify-center.py-12 Transplit
+  v-divider
+  //- v-subheader.justify-center Made by
+  v-card-subtitle.d-flex.justify-center.pb-2 Made by
+  v-card-text.d-flex.justify-center.pt-0.pb-7
+    span Tomoaki Ohta.
 </template>
 
 <script>
